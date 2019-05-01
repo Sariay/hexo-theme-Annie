972f113acdbdba818c9dfd5c52b4c1ec8b30f691
Sariay <1261347403@qq.com>
2019-05-01 17:58:54 +0800

    Fix mobie css!

--- scripts/colorquote.js
+++ scripts/colorquote.js
@@ -1,9 +1,26 @@
 /**
- * Colored quote block
- * The code snippet comes from 'https://github.com/ppoffice/hexo-theme-minos/blob/master/scripts/99_tags.js'!
+ * The code snippet refer to 'https://github.com/ppoffice/hexo-theme-minos/blob/master/scripts/99_tags.js'!
+ * Colorquote tag
+ *
+ * Tag syntax: {% colorquote [type] %} content {% endcolorquote %}
  */
 
-hexo.extend.tag.register('colorquote', function (args, content) {
-    var type =  args[0];
-    return '<blockquote class="colorquote ' + type + '">' + content + '</blockquote>';
-}, {ends: true});
+'use strict';
+
+function colorQuote(args, content) {
+  var type =  args[0] || 'info';
+	var colorquote = '';
+
+	colorquote += '<blockquote class="colorquote ' + type.trim() + '">';
+	colorquote += hexo.render.renderSync({
+		text: content,
+		engine: 'markdown'
+	}).split('\n').join('');
+	colorquote += '</blockquote>';
+
+	return colorquote;
+}
+
+hexo.extend.tag.register('colorquote', colorQuote, {
+	ends: true
+});
